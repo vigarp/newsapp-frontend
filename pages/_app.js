4f08242fa1ad2152a62657702f2e2be360053102
vigarp <vigar.8421@outlook.com>
2022-03-15 21:24:32 +0700

    add bootstrap module

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>NewsApp</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
