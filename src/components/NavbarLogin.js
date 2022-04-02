2a7cb6060fe75356e1454f754f3bb15543e863c8
vigarp <vigar.8421@outlook.com>
2022-04-03 06:44:40 +0700

    add fetching data in navbar login components

--- src/components/NavbarLogin.js
+++ src/components/NavbarLogin.js
@@ -1,8 +1,30 @@
+import axios from "axios"
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect } from "react"
+import { decodeToken } from "react-jwt"
+import { useState } from "react"
 import styles from "../styles/components/NavbarLogin.module.css"
+import { useRouter } from "next/router"
 
 const NavbarLogin = () => {
+    const router = useRouter()
+    const [detailData, setDetailData] = useState()
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const tokenUser = localStorage.getItem('token');
+            const userInfo = decodeToken(tokenUser);
+            axios.get(`${process.env.NEXT_PUBLIC_APP_URL_BACKEND}/users/${userInfo.id}`)
+                .then((res) => {
+                    const detailUser = res.data?.data
+                    setDetailData(detailUser)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    }, [])
+
     return (<>
         <nav>
             <div className={`d-flex justify-content-between mx-5 pt-3 pb-5`}>
@@ -23,7 +45,7 @@ const NavbarLogin = () => {
                 </div>
                 <div className={`d-flex pt-2`}>
                     <Image src={require("../assets/icons/notification-navbar.svg").default} alt="" />
-                    <Image src={require("../assets/profile-pict-navbar.png")} alt="" />
+                    <Image onClick={()=> router.push("/main/profile")} className={`rounded rounded-pill ${styles.userPointer}`} src={detailData?.picture || "https://res.cloudinary.com/vigarp/image/upload/v1648941174/newsapp-assets/Facebook-Blank-Photo_rormrb.jpg"} width={43.68} height={43.68} alt="" />
                 </div>
             </div>
             <hr className={`${styles.divider}`} />
